8fd1a84adcd6442f192c789a002e8e71078a8a86
SteveIsGlitched <61105951+Steveuss@users.noreply.github.com>
2022-08-21 16:15:07 +1000

    Update loadstring.lua

--- script/loadstring.lua
+++ script/loadstring.lua
@@ -1,2 +1,2 @@
 -- LOADER | LOADS THE ACTUAL SCRIPT (LOADSTRING)
-loadstring(game:HttpGet("https://uraniumhub.tk/storage/script.lua", true))()
+loadstring(game:HttpGet("https://uraniumhub.ml/storage/script.lua", true))()
